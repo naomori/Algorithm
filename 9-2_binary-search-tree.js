968f35d98b8229151712ae7c3916928e1836c4c6
naomori <naoki.morita@gmail.com>
2025-04-09 21:38:36 +0900

    2025-04-09 implement binary search tree

---
+++ 9-2_binary-search-tree.js
@@ -0,0 +1,98 @@
+class Node {
+  constructor(key) {
+    this.key = key;
+    this.parent = null;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+let root = null;
+
+const insert = (key) => {
+  let search = root;
+  let parent = null;
+  let node = new Node(key);
+
+  while (search) {
+    parent = search;
+    if (node.key < search.key) {
+      search = search.left;
+    } else {
+      search = search.right;
+    }
+  }
+  node.parent = parent;
+  if (!parent) {
+    root = node;
+  } else {
+    if (node.key < parent.key) {
+      parent.left = node;
+    } else {
+      parent.right = node;
+    }
+  }
+};
+
+const inorder = (node) => {
+  if (!node) return;
+  inorder(node.left);
+  process.stdout.write(` ${node.key}`);
+  inorder(node.right);
+};
+
+const preorder = (node) => {
+  if (!node) return;
+  process.stdout.write(` ${node.key}`);
+  preorder(node.left);
+  preorder(node.right);
+};
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let m = 0;
+
+rl.on("line", (line) => {
+  if (m == 0) {
+    m = parseInt(line.trim());
+  } else {
+    const instruction = line.trim().split(" ", 2);
+    command = instruction[0];
+    number = Number(instruction[1]);
+    switch (command) {
+      case "insert":
+        insert(number);
+        break;
+      case "print":
+        inorder(root);
+        console.log("");
+        preorder(root);
+        console.log("");
+        break;
+      default:
+        break;
+    }
+
+    m--;
+    if (m == 0) {
+      rl.close();
+    }
+  }
+}).on("close", () => {});
+
+/*
+8
+insert 30
+insert 88
+insert 12
+insert 1
+insert 20
+insert 17
+insert 25
+print
+*/
